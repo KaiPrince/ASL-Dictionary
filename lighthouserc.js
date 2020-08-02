5a182e19acd0f86d339d264e9d3d17e1a8779682
KaiPrince <34746763+KaiPrince@users.noreply.github.com>
2020-08-02 15:56:51 -0400

    lighthouse

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,7 +1,7 @@
 module.exports = {
   ci: {
     collect: {
-      staticDistDir: './dist',
+      staticDistDir: './frontend/dist',
     },
     upload: {
       target: 'temporary-public-storage',
