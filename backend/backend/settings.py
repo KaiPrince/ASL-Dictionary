b3c81b7540294cbc200d6232734164eb56175f42
KaiPrince <34746763+KaiPrince@users.noreply.github.com>
2020-08-03 18:25:10 -0400

    fix typo

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -35,7 +35,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "django-jazzmin",
+    "jazzmin",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
