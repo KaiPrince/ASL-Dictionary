951fa1cfcfd63375dbc33986ebbe9617562956fb
KaiPrince <34746763+KaiPrince@users.noreply.github.com>
2020-08-03 01:15:47 -0400

    Fix whitelist.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -139,7 +139,7 @@ CORS_ORIGIN_WHITELIST = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "https://asl-dictionary.web.app",
-    "https://kaiprince.xyz",
+    "https://asl.kaiprince.xyz",
 ]
 
 MEDIA_URL = "/media/"
