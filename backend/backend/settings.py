0dee5ff32d008710cf5728265a5d549a12b66375
Kai Prince <34746763+KaiPrince@users.noreply.github.com>
2022-01-12 20:42:09 -0500

    Freeze backend deps (#17)
    * Update requirements.txt
    
    * Create runtime.txt
    
    * Update requirements.txt
    
    * Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -13,6 +13,23 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import django_heroku
 
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn="https://06bb704ff2134ca4b6d85554a064a321@o439482.ingest.sentry.io/6146703",
+    integrations=[DjangoIntegration()],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
+
 # Get deployment stage from environment vars
 IN_PRODUCTION = os.getenv("PRODUCTION") == "True"
 
